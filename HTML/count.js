fad07ae4cfa20afc85dfc7a0c679a3fecc9f7702
pramodmaurya959 <pramodmaurya959@gmail.com>
2022-12-12 13:27:00 +0530

    resume

---
+++ HTML/count.js
@@ -0,0 +1,17 @@
+let cntNum = document.getElementById("cnt")
+
+
+    
+
+let count =0;
+
+function increment(){
+   count = count +1;
+   cntNum.innerText=count
+}
+
+function decrement(){
+  count = count -1;
+  cntNum.innerText=count
+}
+
